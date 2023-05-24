3836930d6d6e5bab7e986c49e8f345f9c452bc8f
Lukaspy <lukas.crockett@gmail.com>
2023-05-24 09:55:40 -0600

    export results into csv

--- OGT_rise.m
+++ OGT_rise.m
@@ -1,5 +1,8 @@
 myDir = uigetdir; %gets directory
 myFiles = dir(fullfile(myDir,'*.csv')); %gets all csv files in struct
+output = {zeros(50,2)};
+
+
 for k = 1:length(myFiles)
     baseFileName = myFiles(k).name;
     fullFileName = fullfile(myDir, baseFileName);
@@ -9,5 +12,10 @@ for k = 1:length(myFiles)
     plot(col1, col2)
     hold on
     risetime(col2,col1)
+
+    output{k, 1} = baseFileName;
+    output{k, 2} = risetime(col2,col1);
     disp(fullFileName)
 end    
+
+cell2csv("test.csv", output)
