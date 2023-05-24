565708dda2e531a9c94c3dc7a275b6600f53efad
Lukaspy <lukas.crockett@gmail.com>
2023-05-24 09:03:42 -0600

    Added auto export

--- PulseAndMeasure.js
+++ PulseAndMeasure.js
@@ -6,10 +6,11 @@ if(!('Wavegen' in this) || !('Scope' in this)) throw "Please open a Scope and a 
 Wavegen.Channel1.Mode.text = "Simple";
 Wavegen.Synchronization.text = "Independent"
 Wavegen.Channel1.Simple.Type.text = "Pulse";
-Wavegen.Channel1.Simple.Frequency = 1000;
-Wavegen.Channel1.Simple.Phase = 180;
-Wavegen.States.Repeat.value = 1;
-Wavegen.Channel1.Simple.Amplitude = 3;
+Wavegen.Channel1.Simple.Frequency.value = 1000;
+Wavegen.Channel1.Simple.Phase.value = 180;
+Wavegen.Channel1.States.Auto.value = 1;
+Wavegen.Channel1.States.Repeat.value = 1;
+Wavegen.Channel1.Simple.Amplitude.value = 3.3;
 
 Scope.Trigger.Trigger.text = "Repeated";
 Scope.Trigger.Type.text = "Edge";
@@ -17,4 +18,9 @@ Scope.Trigger.Condition.text = "Either";
 Scope.Trigger.Source.text = "Wavegen 1";
 
 Scope.single();
+wait(.1);
 Wavegen.run();
+Scope1.wait();
+
+
+Scope1.Export("/home/lukas/Documents/CampbellResearch/"+Tool.getText("Filename/directory")+".csv", szView = "", fComments = false, fHeader = true, fLabel = true, fHeaderAsComment = false, szNotes = "")
