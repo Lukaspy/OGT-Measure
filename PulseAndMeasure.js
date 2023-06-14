cfdf0a302d8fcfb3aac1477c7e9a56dd5de289ba
Lukaspy <lukas.crockett@gmail.com>
2023-06-14 13:16:23 -0600

    Added double pulse functionality

--- PulseAndMeasure.js
+++ PulseAndMeasure.js
@@ -17,17 +17,78 @@ Scope.Trigger.Trigger.text = "Repeated";
 Scope.Trigger.Type.text = "Edge";
 Scope.Trigger.Condition.text = "Either";
 Scope.Trigger.Source.text = "Wavegen 1";
-Scope.
 
-var baseFile = Tool.getText("Filename/directory")
+var save_user = Tool.question("Save?");
+if (save_user) {
+    var baseFile = Tool.getSaveFile("Filename/Directoty", "~/Documents/CampbellResearch")//Tool.getText("Filename/directory")
+}
 var repeat =  Tool.getText("Repeat times?")
-for (var i = 1; i <= repeat; i++) {
-    Scope.single();
+var delay = Tool.getText("Delay? (s)")
+print(baseFile)
+
+
+//Pulse width in percentage
+var t1_user = 10;
+var t3_user = 5;
+var t4_user = 1/10;
+
+function pulseAndCapture(save) {
+    Scope1.single();
     wait(.1);
     Wavegen.run();
     Scope1.wait();
-    Scope1.Export("/home/lukas/Documents/CampbellResearch/"+baseFile+i.toString()+".csv", szView = "", fComments = false, fHeader = true, fLabel = true, fHeaderAsComment = false, szNotes = "")
-    wait(1);
+    if (save) {
+        Scope1.Export(baseFile+i.toString()+".csv", szView = "", fComments = false, fHeader = true, fLabel = true, fHeaderAsComment = false, szNotes = "")
+    }
 }
+
+function generatePulse(t1,t3,t4) {
+        generated = [];
+        t1 *= 4096/100;
+        t3 *= 4096/100;
+
+        for (var i = 0; i < t1; i++){
+            generated[Math.round(i)] = 1;
+        }
+        for (var t = t1; t < (t1+(t1*t4)); t++) {
+            generated[Math.round(t)] = 0;
+        }
+
+        for (var i = (t1+(t1*t4)); i < (((t1+(t1*t4) + t3))); i++) {
+            generated[Math.round(i)] = 1;
+        }
+
+        for (var i = (((t1+(t1*t4) + t3))); i <= 4096; i++) {
+            generated[Math.round(i)] = 0;
+        }
+        return generated
+        
+        
+}
+
+
+if (Tool.question("Arb Pulses?")) {
+    Wavegen.Channel1.Mode.text = "Custom";
+    t4_factor = t4_user
+    for (var i = 1; i <= repeat; i++) {
+        print("dt = " + t4_factor);
+        arbPulse = generatePulse(t1_user, t3_user, t4_factor);
+        Wavegen.Custom.set("pulse" + i, arbPulse);
+        Wavegen.Channel1.Mode.text = "Custom";
+        Wavegen.Channel1.Custom.Type.text = "pulse" + i;
+        Wavegen.Channel1.Custom.Amplitude.value = 3.3;
+        Wavegen.Channel1.Custom.Frequency.value = 5;
+        pulseAndCapture(save_user);
+        wait(parseInt(delay));
+        t4_factor *= 2;
+    }
+
+} else{
+    for (var i = 1; i <= repeat; i++) {
+        pulseAndCapture();
+        wait(parseInt(delay));
+    }
+}
+
 
 
